ad838fe1eff3a6c3a28fa24d49b867955b9511d9
Erdem Özkök <erdemozkok@hotmail.com.tr>
2022-02-08 20:46:05 +0300

    News App

---
+++ NewsApp/NewsApp/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Erdem Özkök on 8.02.2022.
+//
+
+import Foundation
+
+struct News : Decodable {
+    
+    let title: String
+    let story: String
+    
+}
---
+++ NewsApp/NewsApp/Service/WebService.swift
@@ -0,0 +1,31 @@
+//
+//  WebService.swift
+//  NewsApp
+//
+//  Created by Erdem Özkök on 8.02.2022.
+//
+
+import Foundation
+
+class WebService {
+    
+    func getNews(url: URL, completion: @escaping ([News]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                let newsArray = try? JSONDecoder().decode([News].self, from: data)
+                
+                if let newsArray = newsArray {
+                    completion(newsArray)
+                }
+                
+            }
+        }.resume()
+        
+    }
+    
+}
