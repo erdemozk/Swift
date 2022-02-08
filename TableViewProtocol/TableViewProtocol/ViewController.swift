192b701232d8d74ee3ea93dd810cff5920a7c330
Erdem Özkök <erdemozkok@hotmail.com.tr>
2022-02-08 20:44:47 +0300

    TableView Protocol

---
+++ TableViewProtocol/TableViewProtocol/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TableViewProtocol
+//
+//  Created by Erdem Özkök on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
